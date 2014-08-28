64ff1ae57645aaf84c6e377b635d3fa11e37d591
Ivan Glautier <ivanglautier@gmail.com>
2014-08-28 09:54:42 +0100

    resolved relative paths

--- public/js/concat.js
+++ public/js/concat.js
@@ -1,31 +1,30 @@
 function getContent(url, div) {
 	$.get(url, function(data) {
-		console.log(data);
-		var newDoc = (new DOMParser).parseFromString(data, "text/html");
-		console.log(newDoc);
-		insertLinks(newDoc, function(doc) {
-			var newDiv = document.createElement('div');
-			newDiv.innerHTML = doc.body.innerHTML;
-			$('#'+div).after(newDiv);
-		});
+		var newDiv = document.createElement('div');
+		newDiv.innerHTML = data;
+		insertLinks(newDiv, function(doc) {
+			$('#'+div).after(doc);
+		}, document.getElementById(div).innerHTML);
 	}, "html");
 }
 
 var x = 0;
-function insertLinks(page, callback) {
 
-	var links = page.getElementsByTagName('a');
-	console.log(links);
+function insertLinks(content, callback, path) {
+
+	var links = content.getElementsByTagName('a');
 	for (var i = 0; i < links.length; i++) {
 		if (links[i].hostname == 'localhost') {
-			var expander = page.createElement('div');
-			expander.innerHTML = 'Expand';
+			links[i].href = 'http://' + links[i].host + path + links[i].pathname;
+			var expander = document.createElement('div');
+			if (!links[i].pathname) expander.innerHTML = '';
+			else expander.innerHTML = links[i].pathname;
 			expander.setAttribute('class', 'expander');
 			expander.setAttribute('id', 'e' + x);
 			expander.setAttribute('onclick', 'getContent("'+links[i].href+'", "e'+x+'")');
 			x++;
-			$(links[i]).after(expander);
+			$(links[i].parentNode).after(expander);
 		}
 	}
-	callback(page);
+	callback(content);
 }
